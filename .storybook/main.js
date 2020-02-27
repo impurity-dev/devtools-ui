fcbda9f3ddeed509befcac66570e4a90980fe49e
Tyler Kokoszka <techkoko77@gmail.com>
2020-02-27 00:37:01 -0500

    Added storybook to generic src

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../projects/devtools-ui-storybook/stories/**/*.stories.ts'],
+  stories: ['../src/stories/**/*.stories.ts'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
 };
