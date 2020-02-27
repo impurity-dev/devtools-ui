c731f2b73a1999d8563fd2990fec1424e9def1e8
Tyler Kokoszka <techkoko77@gmail.com>
2020-02-27 01:00:23 -0500

    Solidify storybook setup with library

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../src/stories/**/*.stories.ts'],
+  stories: ['../projects/**/src/stories/**/*.stories.ts'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
 };
--- projects/impurity/devtools-ui/src/stories/test.stories.ts
+++ projects/impurity/devtools-ui/src/stories/test.stories.ts
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/angular';
-import { DevtoolsUiComponent } from '../../projects/impurity/devtools-ui/src/lib/devtools-ui.component';
+import { DevtoolsUiComponent } from '../lib/devtools-ui.component';
 
 storiesOf('Link', module)
   .add('Test', () => ({
