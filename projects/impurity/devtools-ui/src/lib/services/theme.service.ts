170f2388c811490b4b2dd5afc049af67c72f051e
Tyler Kokoszka <techkoko77@gmail.com>
2020-03-04 23:40:40 -0500

    Lint

--- projects/impurity/devtools-ui/src/lib/services/theme.service.ts
+++ projects/impurity/devtools-ui/src/lib/services/theme.service.ts
@@ -4,8 +4,4 @@ import { Injectable } from '@angular/core';
 @Injectable({
     providedIn: 'root',
 })
-export class ThemeService {
-    public changeTheme(_theme: Theme) {
-        throw new Error('Not Implemented!');
-    }
-}
+export class ThemeService {}
