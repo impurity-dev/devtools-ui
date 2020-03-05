bffc7830ebc3be2abaab3cc1a7c3d4989b81b81d
Tyler Kokoszka <techkoko77@gmail.com>
2020-03-04 20:11:43 -0500

    Add theme service

--- projects/storybook/mock/icon.mock.ts
+++
@@ -1,6 +0,0 @@
-import { Icon } from '../../impurity/devtools-ui/src/lib/link-ui/models/icon.model';
-
-export const icon75x75: Icon = {
-    image: 'https://via.placeholder.com/75x75',
-    alt: 'image alt',
-};
