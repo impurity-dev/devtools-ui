d1f7c08a62cdb8c8edc174ac2af5c7334193d615
Tyler Kokoszka <techkoko77@gmail.com>
2020-05-14 16:18:30 -0400

    Cleanup modules

--- projects/impurity/devtools-ui/src/lib/pages/health-page/health-page.component.spec.ts
+++ projects/impurity/devtools-ui/src/lib/pages/health-page/health-page.component.spec.ts
@@ -1,25 +1,25 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HealthPageComponent } from './health-page.component';
 
 describe('HealthPageComponent', () => {
-  let component: HealthPageComponent;
-  let fixture: ComponentFixture<HealthPageComponent>;
+    let component: HealthPageComponent;
+    let fixture: ComponentFixture<HealthPageComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HealthPageComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [HealthPageComponent],
+            imports: [RouterTestingModule],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HealthPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HealthPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- projects/impurity/devtools-ui/src/lib/pages/health-page/health-page.module.ts
+++ projects/impurity/devtools-ui/src/lib/pages/health-page/health-page.module.ts
@@ -1,28 +1,16 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './../../shared/shared.module';
-import { ContainersModule } from './../../containers/containers.module';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HealthPageComponent } from './health-page.component';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContainersModule } from './../../containers/containers.module';
+import { SharedModule } from './../../shared/shared.module';
 import { HealthCardListComponent } from './components/health-card-list/health-card-list.component';
 import { HealthCardComponent } from './components/health-card/health-card.component';
+import { HealthPageComponent } from './health-page.component';
 
 @NgModule({
     declarations: [HealthPageComponent, HealthCardListComponent, HealthCardComponent],
-    imports: [
-        BrowserAnimationsModule,
-        CommonModule,
-        SharedModule,
-        ContainersModule,
-        MatCardModule,
-        MatDividerModule,
-        MatButtonModule,
-        MatIconModule,
-    ],
+    imports: [BrowserAnimationsModule, CommonModule, SharedModule, ContainersModule, MatCardModule],
     exports: [HealthPageComponent],
     schemas: [NO_ERRORS_SCHEMA],
 })
