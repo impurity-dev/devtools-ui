f7ac96b0724de169696e78cabcca4c32643e02aa
Tyler Kokoszka <techkoko77@gmail.com>
2020-05-15 01:23:34 -0400

    Implements onINit

--- projects/impurity/devtools-ui/src/lib/devtools-ui.component.ts
+++ projects/impurity/devtools-ui/src/lib/devtools-ui.component.ts
@@ -1,5 +1,5 @@
 import { SideNavItem } from './navigations/side-nav/models/side-nav-item.model';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Page } from './pages/models/page.model';
 import Image from './shared/models/image.model';
 
@@ -8,7 +8,7 @@ import Image from './shared/models/image.model';
     templateUrl: './devtools-ui.component.html',
     styleUrls: ['./devtools-ui.component.scss'],
 })
-export class DevtoolsUiComponent {
+export class DevtoolsUiComponent implements OnInit {
     @Input() title: string;
     @Input() icon: Image;
     @Input() pages: Page[];
