231f036ece2ba80abcdd4b56888c134aa311d346
Tyler Kokoszka <techkoko77@gmail.com>
2020-05-15 01:11:25 -0400

    Better link structure

--- projects/impurity/devtools-ui/src/lib/navigations/side-nav/side-nav.module.ts
+++ projects/impurity/devtools-ui/src/lib/navigations/side-nav/side-nav.module.ts
@@ -1,15 +1,16 @@
-import { RouterModule } from '@angular/router';
-import { SharedModule } from './../../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatRippleModule } from '@angular/material/core';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from './../../shared/shared.module';
 import { SideNavItemListComponent } from './components/side-nav-item-list/side-nav-item-list.component';
 import { SideNavItemComponent } from './components/side-nav-item/side-nav-item.component';
 import { SideNavComponent } from './side-nav.component';
 
 @NgModule({
     declarations: [SideNavComponent, SideNavItemComponent, SideNavItemListComponent],
-    imports: [CommonModule, SharedModule, RouterModule, MatListModule],
+    imports: [CommonModule, SharedModule, RouterModule, MatListModule, MatRippleModule],
     exports: [SideNavComponent],
 })
 export class SideNavModule {}
